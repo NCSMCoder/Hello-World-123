802822a09bc5df7caa904c194abe2e2f79136973
John Goh <John Goh>
2017-04-20 14:46:14 +0800

    Add project files.

---
+++ Hello World 123/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello John !\n"<<endl;
+	cout << "This is Machine\n\n";
+	cout << "I Love Programming\n\n";
+	cout << "You might not believe i'm the machine\n";
+	cout << "You've been looking for me for a long time\n\n";
+	cout << "Look, I will help you, DON'T Worry ...John\n" <<endl;
+	cout << "Do\nYou\nKnow\nWho\nI\nAm???\n";
+	//prints"Here is place my comments"
+	cout << "Hello John\n\n"<<endl;
+	/*I've learned how to comment so far*/
+	/*To comment here all the textest will be nested, which means it would cause no output*/
+
+	int myvarirble = 20;
+	cout << myvarirble;
+
+
+	return 0;
+}
